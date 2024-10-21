0a3c22f086aa38eecd7ae5c847453f8ae80f3cd7
Kyle Barron <kyle@developmentseed.org>
2024-10-21 00:35:05 -0400

    Delete list of objects

--- object-store-rs/python/object_store_rs/_delete.pyi
+++ object-store-rs/python/object_store_rs/_delete.pyi
@@ -1,14 +1,16 @@
+from typing import Sequence
+
 from .store import ObjectStore
 
-def delete(store: ObjectStore, location: str) -> None:
-    """Delete the object at the specified location.
+def delete(store: ObjectStore, locations: str | Sequence[str]) -> None:
+    """Delete the object at the specified location(s).
 
     Args:
         store: The ObjectStore instance to use.
-        location: The path within ObjectStore to delete.
+        locations: The path or paths within ObjectStore to delete.
     """
 
-async def delete_async(store: ObjectStore, location: str) -> None:
+async def delete_async(store: ObjectStore, locations: str | Sequence[str]) -> None:
     """Call `delete` asynchronously.
 
     Refer to the documentation for [delete][object_store_rs.delete].
