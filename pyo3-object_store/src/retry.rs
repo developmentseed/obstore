4c15a60400588427b16b05e46db432626d90740f
Kyle Barron <kyle@developmentseed.org>
2025-02-03 18:44:16 -0500

    Fix parsing

--- pyo3-object_store/src/retry.rs
+++ pyo3-object_store/src/retry.rs
@@ -1,56 +1,99 @@
 use std::time::Duration;
 
 use object_store::{BackoffConfig, RetryConfig};
+use pyo3::intern;
 use pyo3::prelude::*;
 
-#[derive(Clone, Debug, FromPyObject, IntoPyObject)]
+#[derive(Clone, Debug, IntoPyObject)]
 pub struct PyBackoffConfig {
     #[pyo3(item)]
-    init_backoff: Option<Duration>,
+    init_backoff: Duration,
     #[pyo3(item)]
-    max_backoff: Option<Duration>,
+    max_backoff: Duration,
     #[pyo3(item)]
-    base: Option<f64>,
+    base: f64,
+}
+
+impl<'py> FromPyObject<'py> for PyBackoffConfig {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        let mut backoff_config = BackoffConfig::default();
+        let py = ob.py();
+        if let Ok(init_backoff) = ob.get_item(intern!(py, "init_backoff")) {
+            backoff_config.init_backoff = init_backoff.extract()?;
+        }
+        if let Ok(max_backoff) = ob.get_item(intern!(py, "max_backoff")) {
+            backoff_config.max_backoff = max_backoff.extract()?;
+        }
+        if let Ok(base) = ob.get_item(intern!(py, "base")) {
+            backoff_config.base = base.extract()?;
+        }
+        Ok(backoff_config.into())
+    }
 }
 
 impl From<PyBackoffConfig> for BackoffConfig {
     fn from(value: PyBackoffConfig) -> Self {
-        let mut backoff_config = BackoffConfig::default();
-        if let Some(init_backoff) = value.init_backoff {
-            backoff_config.init_backoff = init_backoff;
+        BackoffConfig {
+            init_backoff: value.init_backoff,
+            max_backoff: value.max_backoff,
+            base: value.base,
         }
-        if let Some(max_backoff) = value.max_backoff {
-            backoff_config.max_backoff = max_backoff;
-        }
-        if let Some(base) = value.base {
-            backoff_config.base = base;
-        }
-        backoff_config
     }
 }
 
-#[derive(Clone, Debug, FromPyObject, IntoPyObject)]
+impl From<BackoffConfig> for PyBackoffConfig {
+    fn from(value: BackoffConfig) -> Self {
+        PyBackoffConfig {
+            init_backoff: value.init_backoff,
+            max_backoff: value.max_backoff,
+            base: value.base,
+        }
+    }
+}
+
+#[derive(Clone, Debug, IntoPyObject)]
 pub struct PyRetryConfig {
     #[pyo3(item)]
-    backoff: Option<PyBackoffConfig>,
+    backoff: PyBackoffConfig,
     #[pyo3(item)]
-    max_retries: Option<usize>,
+    max_retries: usize,
     #[pyo3(item)]
-    retry_timeout: Option<Duration>,
+    retry_timeout: Duration,
+}
+
+impl<'py> FromPyObject<'py> for PyRetryConfig {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        let mut retry_config = RetryConfig::default();
+        let py = ob.py();
+        if let Ok(backoff) = ob.get_item(intern!(py, "backoff")) {
+            retry_config.backoff = backoff.extract::<PyBackoffConfig>()?.into();
+        }
+        if let Ok(max_retries) = ob.get_item(intern!(py, "max_retries")) {
+            retry_config.max_retries = max_retries.extract()?;
+        }
+        if let Ok(retry_timeout) = ob.get_item(intern!(py, "retry_timeout")) {
+            retry_config.retry_timeout = retry_timeout.extract()?;
+        }
+        Ok(retry_config.into())
+    }
 }
 
 impl From<PyRetryConfig> for RetryConfig {
     fn from(value: PyRetryConfig) -> Self {
-        let mut retry_config = RetryConfig::default();
-        if let Some(backoff) = value.backoff {
-            retry_config.backoff = backoff.into();
+        RetryConfig {
+            backoff: value.backoff.into(),
+            max_retries: value.max_retries,
+            retry_timeout: value.retry_timeout,
         }
-        if let Some(max_retries) = value.max_retries {
-            retry_config.max_retries = max_retries;
+    }
+}
+
+impl From<RetryConfig> for PyRetryConfig {
+    fn from(value: RetryConfig) -> Self {
+        PyRetryConfig {
+            backoff: value.backoff.into(),
+            max_retries: value.max_retries,
+            retry_timeout: value.retry_timeout,
         }
-        if let Some(retry_timeout) = value.retry_timeout {
-            retry_config.retry_timeout = retry_timeout;
-        }
-        retry_config
     }
 }
