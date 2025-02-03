1e430be8f6f5e1c00385f92375d2c517e02577c1
Kyle Barron <kyle@developmentseed.org>
2025-02-03 16:53:59 -0500

    Allow passing in partial retry/backoff config

--- pyo3-object_store/src/retry.rs
+++ pyo3-object_store/src/retry.rs
@@ -6,39 +6,51 @@ use pyo3::prelude::*;
 #[derive(Clone, Debug, FromPyObject, IntoPyObject)]
 pub struct PyBackoffConfig {
     #[pyo3(item)]
-    init_backoff: Duration,
+    init_backoff: Option<Duration>,
     #[pyo3(item)]
-    max_backoff: Duration,
+    max_backoff: Option<Duration>,
     #[pyo3(item)]
-    base: f64,
+    base: Option<f64>,
 }
 
 impl From<PyBackoffConfig> for BackoffConfig {
     fn from(value: PyBackoffConfig) -> Self {
-        BackoffConfig {
-            init_backoff: value.init_backoff,
-            max_backoff: value.max_backoff,
-            base: value.base,
+        let mut backoff_config = BackoffConfig::default();
+        if let Some(init_backoff) = value.init_backoff {
+            backoff_config.init_backoff = init_backoff;
         }
+        if let Some(max_backoff) = value.max_backoff {
+            backoff_config.max_backoff = max_backoff;
+        }
+        if let Some(base) = value.base {
+            backoff_config.base = base;
+        }
+        backoff_config
     }
 }
 
 #[derive(Clone, Debug, FromPyObject, IntoPyObject)]
 pub struct PyRetryConfig {
     #[pyo3(item)]
-    backoff: PyBackoffConfig,
+    backoff: Option<PyBackoffConfig>,
     #[pyo3(item)]
-    max_retries: usize,
+    max_retries: Option<usize>,
     #[pyo3(item)]
-    retry_timeout: Duration,
+    retry_timeout: Option<Duration>,
 }
 
 impl From<PyRetryConfig> for RetryConfig {
     fn from(value: PyRetryConfig) -> Self {
-        RetryConfig {
-            backoff: value.backoff.into(),
-            max_retries: value.max_retries,
-            retry_timeout: value.retry_timeout,
+        let mut retry_config = RetryConfig::default();
+        if let Some(backoff) = value.backoff {
+            retry_config.backoff = backoff.into();
         }
+        if let Some(max_retries) = value.max_retries {
+            retry_config.max_retries = max_retries;
+        }
+        if let Some(retry_timeout) = value.retry_timeout {
+            retry_config.retry_timeout = retry_timeout;
+        }
+        retry_config
     }
 }
