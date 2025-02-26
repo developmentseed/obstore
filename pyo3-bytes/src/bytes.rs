2b7a3cefb1966e044dd6dfef684e00295ae9252a
jessekrubin <jessekrubin@gmail.com>
2025-02-26 08:31:10 -0800

    fix: indices typo

--- pyo3-bytes/src/bytes.rs
+++ pyo3-bytes/src/bytes.rs
@@ -76,11 +76,11 @@ impl PyBytes {
     fn slice(&self, slice: &Bound<'_, PySlice>) -> PyResult<PyBytes> {
         let bytes_length = self.0.len() as isize;
         let (start, stop, step) = {
-            let slice_indicies = slice.indices(bytes_length)?;
+            let slice_indices = slice.indices(bytes_length)?;
             (
-                slice_indicies.start,
-                slice_indicies.stop,
-                slice_indicies.step,
+                slice_indices.start,
+                slice_indices.stop,
+                slice_indices.step,
             )
         };
 
