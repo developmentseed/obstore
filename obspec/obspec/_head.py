6d15d7893066a6e2c69696b9ca1d05736ec0a7a0
Kyle Barron <kyle@developmentseed.org>
2025-03-07 13:32:06 -0500

    Obspec type specification

---
+++ obspec/obspec/_head.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from ._meta import ObjectMeta
+
+
+class Head(Protocol):
+    def head(self, path: str) -> ObjectMeta:
+        """Return the metadata for the specified location.
+
+        Args:
+            store: The ObjectStore instance to use.
+            path: The path within ObjectStore to retrieve.
+
+        Returns:
+            ObjectMeta
+
+        """
+        ...
+
+
+class HeadAsync(Protocol):
+    async def head_async(self, path: str) -> ObjectMeta:
+        """Call `head` asynchronously.
+
+        Refer to the documentation for [head][obstore.head].
+        """
+        ...
