51a11d77af5524c1dfe10c22db271abb83dccca4
Kyle Barron <kyle@developmentseed.org>
2025-03-11 17:35:33 -0400

    Convert tests to use store api

--- tests/test_put.py
+++ tests/test_put.py
@@ -2,7 +2,6 @@ import itertools
 
 import pytest
 
-import obstore as obs
 from obstore.exceptions import AlreadyExistsError
 from obstore.store import MemoryStore
 
@@ -10,8 +9,8 @@ from obstore.store import MemoryStore
 def test_put_non_multipart():
     store = MemoryStore()
 
-    obs.put(store, "file1.txt", b"foo", use_multipart=False)
-    assert obs.get(store, "file1.txt").bytes() == b"foo"
+    store.put("file1.txt", b"foo", use_multipart=False)
+    assert store.get("file1.txt").bytes() == b"foo"
 
 
 def test_put_non_multipart_sync_iterable():
@@ -19,8 +18,8 @@ def test_put_non_multipart_sync_iterable():
 
     b = b"the quick brown fox jumps over the lazy dog,"
     iterator = itertools.repeat(b, 5)
-    obs.put(store, "file1.txt", iterator, use_multipart=False)
-    assert obs.get(store, "file1.txt").bytes() == (b * 5)
+    store.put("file1.txt", iterator, use_multipart=False)
+    assert store.get("file1.txt").bytes() == (b * 5)
 
 
 @pytest.mark.asyncio
@@ -33,15 +32,15 @@ async def test_put_non_multipart_async_iterable():
         for _ in range(5):
             yield b"the quick brown fox jumps over the lazy dog,"
 
-    await obs.put_async(store, "file1.txt", it(), use_multipart=False)
-    assert obs.get(store, "file1.txt").bytes() == (b * 5)
+    await store.put_async("file1.txt", it(), use_multipart=False)
+    assert store.get("file1.txt").bytes() == (b * 5)
 
 
 def test_put_multipart_one_chunk():
     store = MemoryStore()
 
-    obs.put(store, "file1.txt", b"foo", use_multipart=True)
-    assert obs.get(store, "file1.txt").bytes() == b"foo"
+    store.put("file1.txt", b"foo", use_multipart=True)
+    assert store.get("file1.txt").bytes() == b"foo"
 
 
 def test_put_multipart_large():
@@ -50,20 +49,20 @@ def test_put_multipart_large():
     data = b"the quick brown fox jumps over the lazy dog," * 5000
     path = "big-data.txt"
 
-    obs.put(store, path, data, use_multipart=True)
-    assert obs.get(store, path).bytes() == data
+    store.put(path, data, use_multipart=True)
+    assert store.get(path).bytes() == data
 
 
 def test_put_mode():
     store = MemoryStore()
 
-    obs.put(store, "file1.txt", b"foo")
-    obs.put(store, "file1.txt", b"bar", mode="overwrite")
+    store.put("file1.txt", b"foo")
+    store.put("file1.txt", b"bar", mode="overwrite")
 
     with pytest.raises(AlreadyExistsError):
-        obs.put(store, "file1.txt", b"foo", mode="create")
+        store.put("file1.txt", b"foo", mode="create")
 
-    assert obs.get(store, "file1.txt").bytes() == b"bar"
+    assert store.get("file1.txt").bytes() == b"bar"
 
 
 @pytest.mark.asyncio
@@ -73,14 +72,14 @@ async def test_put_async_iterable():
     data = b"the quick brown fox jumps over the lazy dog," * 50_000
     path = "big-data.txt"
 
-    await obs.put_async(store, path, data)
+    await store.put_async(path, data)
 
-    resp = await obs.get_async(store, path)
+    resp = await store.get_async(path)
     stream = resp.stream(min_chunk_size=0)
     new_path = "new-path.txt"
-    await obs.put_async(store, new_path, stream)
+    await store.put_async(new_path, stream)
 
-    assert obs.get(store, new_path).bytes() == data
+    assert store.get(new_path).bytes() == data
 
 
 def test_put_sync_iterable():
@@ -91,6 +90,6 @@ def test_put_sync_iterable():
     data = b * 50_000
     path = "big-data.txt"
 
-    obs.put(store, path, iterator)
+    store.put(path, iterator)
 
-    assert obs.get(store, path).bytes() == data
+    assert store.get(path).bytes() == data
