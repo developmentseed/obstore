b636925ebac654064ae042ef6a38dcc6ef5a4fe5
Kyle Barron <kyle@developmentseed.org>
2025-03-24 11:32:02 -0400

    Add pyright type checking to ci (#388)
    * Add pyright to ci
    
    * progress fixing ci
    
    * skip fsspec typing
    
    * type checks
    
    * fix from_url typing

--- tests/store/test_gcs.py
+++ tests/store/test_gcs.py
@@ -6,10 +6,10 @@ from obstore.store import GCSStore
 
 def test_overlapping_config_keys():
     with pytest.raises(BaseError, match="Duplicate key"):
-        GCSStore(google_bucket="bucket", GOOGLE_BUCKET="bucket")
+        GCSStore(google_bucket="bucket", GOOGLE_BUCKET="bucket")  # type: ignore intentional test
 
     with pytest.raises(BaseError, match="Duplicate key"):
-        GCSStore(config={"google_bucket": "test", "GOOGLE_BUCKET": "test"})
+        GCSStore(config={"google_bucket": "test", "GOOGLE_BUCKET": "test"})  # type: ignore intentional test
 
 
 def test_eq():
