3abffb65f0102ff7724212d1a5e06fd6aa729a33
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-25 08:20:22 -0600

    chore: add failing test to demonstrate json.loads(bytes)

--- tests/test_bytes.py
+++ tests/test_bytes.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import pickle
 from typing import TYPE_CHECKING
 
@@ -99,3 +100,8 @@ class TestBytesSlice:
 def test_pickle():
     b = Bytes(b"hello_world")
     assert b == pickle.loads(pickle.dumps(b))
+
+
+def test_json_loads():
+    b = Bytes(b"hello world")
+    json.loads(b)
