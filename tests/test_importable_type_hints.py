b36881b42348980a580f044b8da858c12075fe5a
Kyle Barron <kyle@developmentseed.org>
2026-01-19 13:07:24 -0500

    Add test

---
+++ tests/test_importable_type_hints.py
@@ -0,0 +1,11 @@
+def test_typed_dicts_should_be_importable():
+    from obstore import (
+        Attribute,  # noqa: F401
+        Attributes,  # noqa: F401
+        GetOptions,  # noqa: F401
+        ListChunkType,  # noqa: F401
+        ListResult,  # noqa: F401
+        ObjectMeta,  # noqa: F401
+        OffsetRange,  # noqa: F401
+        SuffixRange,  # noqa: F401
+    )
