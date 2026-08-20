26d7edf53e764558b8091395d241a8d1225a6bc3
Kyle Barron <kyle@developmentseed.org>
2026-08-20 19:23:49 -0400

    keep stub in sync with store.py

--- obstore/python/obstore/_put.pyi
+++ obstore/python/obstore/_put.pyi
@@ -109,7 +109,7 @@ def put(
     tags: dict[str, str] | None = None,
     mode: PutMode | None = None,
     use_multipart: bool | None = None,
-    chunk_size: int = 5242880,
+    chunk_size: int = 5 * 1024 * 1024,
     max_concurrency: int = 12,
 ) -> PutResult:
     """Save the provided bytes to the specified location.
@@ -179,7 +179,7 @@ async def put_async(
     tags: dict[str, str] | None = None,
     mode: PutMode | None = None,
     use_multipart: bool | None = None,
-    chunk_size: int = 5242880,
+    chunk_size: int = 5 * 1024 * 1024,
     max_concurrency: int = 12,
 ) -> PutResult:
     """Call `put` asynchronously.
